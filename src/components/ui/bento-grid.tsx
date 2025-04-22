af3135d574ea6bd524a0d8d7cd03d508269170e7
ujjwal bhaedwaj <ujjwalbhardwaj.2002>
2025-04-22 12:45:20 +0530

    final commit

--- src/components/ui/bento-grid.tsx
+++ src/components/ui/bento-grid.tsx
@@ -50,7 +50,7 @@ export const BentoGridItem = ({
   spareImg?: string;
 }) => {
   const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const rightLists = ["MySQL", "NextJS", "MongoDB"];
 
   const [copied, setCopied] = useState(false);
 
